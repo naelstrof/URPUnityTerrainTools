435f703dc60a386b4ef8e4f373eb025235f35192
Naelstrof <naelstrof@gmail.com>
2021-07-05 04:17:39 -0600

    Removed/ignored logs, added basic top-down texture splatting via brushes.

---
+++ Brushes/Brush.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace TerrainBrush {
+    // Made to be inherited.
+    public abstract class Brush : MonoBehaviour {
+        public virtual Bounds brushBounds {
+            get {
+                return new Bounds(transform.position, transform.lossyScale);
+            }
+        }
+        public abstract void Execute(CommandBuffer cmd, RenderTargetHandle renderTarget, TerrainBrushVolume texture);
+        public virtual void OnDrawGizmosSelected() {
+            Gizmos.DrawCube(brushBounds.center, brushBounds.size);
+        }
+    }
+}

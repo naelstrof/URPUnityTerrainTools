b456616bc3b251b8a7f264c67bb40d39e5f96e4f
Naelstrof <naelstrof@gmail.com>
2021-07-05 21:46:03 -0600

    Path renderer!

--- Brushes/PathBrush.cs
+++ Brushes/PathBrush.cs
@@ -6,10 +6,15 @@ using UnityEngine.Rendering.Universal;
 
 namespace TerrainBrush {
     public class PathBrush : Brush {
-        float pointsPerMeter = 1f;
+        [Range(0f,512f)]
+        public float softRadius = 16f;
+        [Range(0.5f,2f)]
+        public float pointsPerMeter = 1f;
         [SerializeField][HideInInspector]
         private LineRenderer line;
         public bool closedLoop = false;
+        public ComputeShader eikonalShader;
+        public Material pathBlit;
         //public List<Transform> pathNodes = new List<Transform>();
         private List<JPBotelho.CatmullRom.CatmullFactors> factors;
         private float maxWidth {
@@ -18,7 +23,7 @@ namespace TerrainBrush {
                 foreach( var key in line.widthCurve.keys) {
                     maxWidth = Mathf.Max(key.value, maxWidth);
                 }
-                return maxWidth;
+                return maxWidth+softRadius*0.5f;
             }
         }
         public override Bounds brushBounds {
@@ -43,11 +48,38 @@ namespace TerrainBrush {
                 return b;
             }
         }
-        public override void Execute(CommandBuffer cmd, RenderTargetHandle renderTarget, TerrainBrushVolume volume) {
+        public override void Execute(CommandBuffer cmd, RenderTargetHandle renderTarget, TerrainBrushVolume volume, Matrix4x4 view, Matrix4x4 projection) {
             OnValidate();
+            // Now prepare a texture with our line rendered to it.
+            RenderTargetHandle temporaryTextureA = new RenderTargetHandle();
+            RenderTargetHandle temporaryTextureB = new RenderTargetHandle();
+            temporaryTextureA.Init("_TerrainBrushSDFTemporaryTextureA");
+            temporaryTextureB.Init("_TerrainBrushSDFTemporaryTextureB");
+            cmd.GetTemporaryRT(temporaryTextureA.id, volume.texture.width, volume.texture.height, 0, FilterMode.Point, UnityEngine.Experimental.Rendering.GraphicsFormat.R32_SFloat, 1, true, RenderTextureMemoryless.None, false);
+            cmd.GetTemporaryRT(temporaryTextureB.id, volume.texture.width, volume.texture.height, 0, FilterMode.Point, UnityEngine.Experimental.Rendering.GraphicsFormat.R32_SFloat, 1, true, RenderTextureMemoryless.None, false);
+            cmd.SetRenderTarget(temporaryTextureA.id);
+            cmd.ClearRenderTarget(true, true, Color.white);
             Mesh tempMesh = new Mesh();
             line.BakeMesh(tempMesh, true);
-            cmd.DrawMesh(tempMesh,Matrix4x4.identity,line.sharedMaterials[0], 0, 0);
+            cmd.DrawMesh(tempMesh, Matrix4x4.identity, line.sharedMaterials[0], 0, 0);
+
+            int threadGroupsX = Mathf.FloorToInt(volume.texture.width / 8.0f);
+            int threadGroupsY = Mathf.FloorToInt(volume.texture.height / 8.0f);
+            for(int i=0;i<256;i++) {
+                cmd.SetComputeTextureParam(eikonalShader, 0, "Source", temporaryTextureA.id);
+                cmd.SetComputeTextureParam(eikonalShader, 0, "Result", temporaryTextureB.id);
+                cmd.DispatchCompute(eikonalShader, 0, threadGroupsX, threadGroupsY, 1);
+
+                cmd.SetComputeTextureParam(eikonalShader, 0, "Source", temporaryTextureB.id);
+                cmd.SetComputeTextureParam(eikonalShader, 0, "Result", temporaryTextureA.id);
+                cmd.DispatchCompute(eikonalShader, 0, threadGroupsX, threadGroupsY, 1);
+            }
+            pathBlit.SetFloat("_Radius", softRadius);
+            cmd.Blit(temporaryTextureA.id, renderTarget.id, pathBlit);
+            cmd.ReleaseTemporaryRT(temporaryTextureA.id);
+            cmd.ReleaseTemporaryRT(temporaryTextureB.id);
+            cmd.SetRenderTarget(renderTarget.id);
+
             //cmd.DrawRenderer(GetComponent<LineRenderer>(), GetComponent<LineRenderer>().sharedMaterial);
         }
 
@@ -67,8 +99,9 @@ namespace TerrainBrush {
             factors = JPBotelho.CatmullRom.GenerateCatmullFactors(pathNodes,closedLoop);
             // Precalculate the needed points
             int totalPoints = 0;
-            for(int i=0;i<pathNodes.Count-1;i++) {
-                float len = Vector3.Distance(pathNodes[i].position, pathNodes[i+1].position);
+            int pointAdjustment = closedLoop ? 0 : 1;
+            for(int i=0;i<pathNodes.Count-pointAdjustment;i++) {
+                float len = Vector3.Distance(pathNodes[i%pathNodes.Count].position, pathNodes[(i+1)%pathNodes.Count].position);
                 for (float t=0;t<1f;t+=pointsPerMeter/len) {
                     totalPoints++;
                 }
@@ -76,8 +109,8 @@ namespace TerrainBrush {
             line.positionCount = totalPoints;
             // Now calculate them for real
             int currentPoint = 0;
-            for(int i=0;i<pathNodes.Count-1;i++) {
-                float len = Vector3.Distance(pathNodes[i].position, pathNodes[i+1].position);
+            for(int i=0;i<pathNodes.Count-pointAdjustment;i++) {
+                float len = Vector3.Distance(pathNodes[i%pathNodes.Count].position, pathNodes[(i+1)%pathNodes.Count].position);
                 for (float t=0;t<1f;t+=pointsPerMeter/len) {
                     line.SetPosition(currentPoint++, JPBotelho.CatmullRom.CalculatePosition(factors[i].p1, factors[i].p2, factors[i].p3, factors[i].p4, t));
                 }
@@ -95,8 +128,8 @@ namespace TerrainBrush {
                     OnValidate();
                     t.hasChanged = false;
                 }
-                float mw = maxWidth;
-                Gizmos.DrawWireSphere(t.position, mw);
+                //float mw = maxWidth;
+                //Gizmos.DrawWireSphere(t.position, mw*0.5f);
             }
             //Gizmos.DrawWireCube(brushBounds.center, brushBounds.size);
         }
